f7711e6acc2113d1fd2535e02e38effb62e688a4
Sam Bengtson <sambeng1@gmail.com>
2019-01-14 10:59:51 -0600

    finished bubbleSort

--- bubblesort.js
+++ bubblesort.js
@@ -1,4 +1,18 @@
 function bubbleSort(arr) {
-    if (arr.length <= 1)
+    
+    if (arr.length <= 1){
         return arr;
+    }
+
+    for (let i = arr.length - 1 ; i > 0; i--){
+        for (let j = 0; j < i; j++){
+            let first = arr[j];
+            let second = arr[j+1];
+            if (second < first){
+                arr[j] = second;
+                arr[j+1] = first
+            }
+        }
+    }
+    return arr;
 }
